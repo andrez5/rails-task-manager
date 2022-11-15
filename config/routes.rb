be1c5e70830f61a12db280e424da21766eb5448d
Felipe Andrez Gonzalez <felipeandrez@gmail.com>
2022-11-15 14:10:39 -0500

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   # READ all tasks
   get 'tasks', to: 'tasks#index'
+
+  # CREATE a task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new'
+
+  # READ one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # UPDATE a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # DELETE a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
